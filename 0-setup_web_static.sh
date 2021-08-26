ea47fcf9d61ff5607c8a6062e330c03549706dc2
SebastianMH14 <sebasmh2002@hotmail.com>
2021-08-26 13:44:41 -0500

    add chmod

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,6 +10,6 @@ touch /data/web_static/releases/test/index.html
 echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo chgrp -R ubuntu /data/
+sudo chmod -R 775 /data/
 sudo sed -i '57 i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
