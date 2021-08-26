21c973686b3306f06f7bc7ba07276ae307c4890f
SebastianMH14 <sebasmh2002@hotmail.com>
2021-08-26 13:22:44 -0500

    shellcheck fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,7 +7,7 @@ fi
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 touch /data/web_static/releases/test/index.html
-sudo echo "Holberton School" > /data/web_static/releases/test/index.html
+echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo chgrp -R ubuntu /data/
