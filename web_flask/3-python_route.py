e126838727c5287330080ca125c64d45cd310b06
SebastianMH14 <sebasmh2002@hotmail.com>
2021-09-11 11:20:11 -0500

    add coments

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -7,25 +7,30 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
+    """index page"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hello():
+    """Hello HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
+    """c is cool"""
     return 'C ' + text.replace('_', ' ')
 
 @app.route('/python/<text>', strict_slashes=False)
 def python(text=None):
-        return 'Python ' + text.replace('_', ' ')
+    """python is text"""
+    return 'Python ' + text.replace('_', ' ')
 
 @app.route('/python/', strict_slashes=False)
 def python2(text="is cool"):
-    return 'Python ' + text.replace('_', ' ')
+    """python is cool"""
+    return 'Python ' + text
 
 
 if __name__ == '__main__':
