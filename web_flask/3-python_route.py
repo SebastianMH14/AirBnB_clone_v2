26246b07552c89f13ac4b148006d55a87b7e1e88
SebastianMH14 <sebasmh2002@hotmail.com>
2021-09-11 10:05:33 -0500

    add task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return 'C ' + text.replace('_', ' ')
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text=None):
+        return 'Python ' + text.replace('_', ' ')
+
+@app.route('/python/', strict_slashes=False)
+def python2():
+    return 'Python is cool'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
