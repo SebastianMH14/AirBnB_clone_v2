0638e45c281a670bfddc9600ec5f9017f81e1a80
SebastianMH14 <sebasmh2002@hotmail.com>
2021-09-11 13:45:54 -0500

    add task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from flask import Flask, escape, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """index page"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """Hello HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """c is cool"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """python is text"""
+    python = text.replace('_', ' ')
+    return 'Python ' + escape(python)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+        return escape(n) + ' is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n=None):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
