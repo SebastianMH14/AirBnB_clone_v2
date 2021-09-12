e34dfe26646ba6d04b3150d5e8017045f2f0fc95
SebastianMH14 <sebasmh2002@hotmail.com>
2021-09-12 17:08:14 -0500

    add task 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    """list all states"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_appcontext():
+    """close the conection with db"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
